78d0da188dae2ca8d155f61e88cef72c4d57ce98
praveshcj <praveshjain500@gmail.com>
2021-01-04 21:48:37 +0530

    Creating new repo

---
+++ React-Portfolio-master/src/resumeData.js
@@ -0,0 +1,84 @@
+let resumeData = {
+    "name": "Pravesh Jain",
+    "role": "Final-Year UG, Department of Computer Science and Engineering, IIT Kharagpur",
+    "linkedinId":"https://www.linkedin.com/in/pravesh-jain-67825714a/",
+    "skypeid": "Your skypeid",
+    "roleDescription": "I like trying out new technologies, including creating new applications, and working with interesting data.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/pravesh-jain-67825714a/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/praveshcj",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"twitter",
+          "url":"https://twitter.com/praveshjain500",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"I am a Final Year Student at Department of Computer Science And Engineering at IIT Kharagpur. I am aspiring Software Engineer, currently working in Data Science",
+    "address":"India",
+    "website":"https://onebyteatatime-portfolio.herokuapp.com/",
+    "education":[
+      {
+        "UniversityName":"Indian Institute of Technology Kharagpur",
+        "specialization":"Computer Science and Engineering",
+        "MonthOfPassing":"May",
+        "YearOfPassing":"2021",
+        "Achievements":"CG : 8.52"
+      },
+    ],
+    "work":[
+      {
+        "CompanyName":"Honeywell Technology Solutions",
+        "specialization":"Intern ",
+        "MonthOfLeaving":"July",
+        "YearOfLeaving":"2020",
+        "Achievements":""
+      },
+      {
+        "CompanyName":"FlexiEle Consulting Pvt. Lmt.",
+        "specialization":"Intern",
+        "MonthOfLeaving":"July",
+        "YearOfLeaving":"2019",
+        "Achievements":""
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      },
+      {
+        "skillname":"Flask"
+      },
+      {
+        "skillname":"Heroku"
+      },
+      {
+        "skillname":"Django"
+      },
+      {
+        "skillname":"MongoDb"
+      }
+    ],
+    "portfolio":[
+      
+    ],
+    "testimonials":[
+      
+    ]
+  }
+  
+  export default resumeData
